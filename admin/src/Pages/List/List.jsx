2538ee2d0ed790a163311e986c38dcaeb5976314
Nihar R Panda <110398042+NiharPanda23@users.noreply.github.com>
2024-11-24 13:48:00 +0530

    Add and delete function copleted

--- admin/src/Pages/List/List.jsx
+++ admin/src/Pages/List/List.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom"
 import "./List.css";
 
 const List = () => {
@@ -12,7 +13,23 @@ const List = () => {
       const response = await axios.get(`http://localhost:3000/api/food/list`);
       if (response.data) {
         setList(response.data);
-        console.log(response.data);
+        // console.log(response.data);
+      }
+    } catch (e) {
+      toast.error(e.message);
+    }
+  };
+
+  const removeFood = async (foodId) => {
+    try {
+      const response = await axios.delete(`http://localhost:3000/api/food/remove`, {
+        data: { id: foodId },
+      });
+      if (response.data.success === true) {
+        setList((prev)=> prev.filter((food) => food._id !== foodId))
+        toast.success(response.data.message);
+      } else {
+        toast.error(response.data.message);
       }
     } catch (e) {
       toast.error(e.message);
@@ -34,23 +51,31 @@ const List = () => {
           <b>Price</b>
           <b>Action</b>
         </div>
-        {
-          list.map((food)=>{
-            return(
-              <div key={food._id} className="list-table-format">
-                <img src={`http://localhost:3000/images/`+ food.image} alt={food.name} />
-                <p>{food.name}</p>
-                <p>{food.category}</p>
-                <p>{food.price}</p>
-                <div className="action">
-                  <img src={assets.Add} alt="Add" />
-                  <img src={assets.update} alt="update" />
-                  <img src={assets.deleteButton} alt="deleteButton" />
-                </div>
+        {list.map((food) => {
+          return (
+            <div key={food._id} className="list-table-format">
+              <img
+                src={`http://localhost:3000/images/` + food.image}
+                alt={food.name}
+              />
+              <p>{food.name}</p>
+              <p>{food.category}</p>
+              <p>{food.price}</p>
+              <div className="action">
+                <Link to='/add' className="link-reset">
+                  <img className="add-img" src={assets.Add} alt="Add" />
+                </Link>
+                <img src={assets.update} alt="update" className="action-img" />
+                <img
+                  onClick={() => removeFood(food._id)}
+                  src={assets.deleteButton}
+                  alt="deleteButton"
+                  className="action-img"
+                />
               </div>
-            )
-          })
-        }
+            </div>
+          );
+        })}
       </div>
     </div>
   );
