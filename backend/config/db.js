2717653c9be998e645a0c7278180a5bed8829fa2
Nihar R Panda <110398042+NiharPanda23@users.noreply.github.com>
2024-12-20 17:17:41 +0530

    Setup for product hosting

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const MONGO_URI = process.env.MONGO_URI;
+
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+         //mongodb+srv://${id}:${password}@cluster0.55gey.mongodb.net/${db}?retryWrites=true&w=majority
+    } catch (error) {
+        console.log("Connection error:", error);
+    }
+};
+
+module.exports = connectDb;
